35af5ec209ffb0010672736eee9e7805e66d97e7
Hitinbajaj <hitinbajaj77@gmail.com>
2024-01-19 04:49:16 +0530

    updated url vercel

--- e-comerce-frontend/src/utils/config.js
+++ e-comerce-frontend/src/utils/config.js
@@ -1,5 +1,5 @@
 const devConfig = {
-  baseURL: process.env.REACT_APP_API_BASE_URL || "https://e-commerce-backend-wnin.onrender.com/",
+  baseURL: process.env.REACT_APP_API_BASE_URL || "https://e-commerce-backend-nine-omega.vercel.app/",
 };
 
 const prodConfig = {
